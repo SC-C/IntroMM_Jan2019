905113abe66c3e3dafdab51e7813d8bf10e0dba3
Shih-chen.Chao <Shih-chen.Chao@manchester.ac.uk>
2019-01-22 10:48:20 +0000

    initialised git and added files to the local repository

---
+++ TestPackage.m
@@ -0,0 +1,18 @@
+(* ::Package:: *)
+
+(* ::This is a test package:: *)
+(**)
+
+
+BeginPackage["testPackage`"];
+
+timesTwo::usage="timesTwo[a, b], return a*b";
+
+Begin["`Private`"];
+
+timesTwo[a_, b_] := Times[a, b];
+
+
+End[];
+
+EndPackage[]; 
